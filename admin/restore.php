91aa420c1bae57a51c3b0d84b9365b1290098828
Kyle Stevenson <kyle.stevenson94@gmail.com>
2013-05-13 13:49:33 -0700

    Protect restore.php

--- admin/restore.php
+++ admin/restore.php
@@ -1,6 +1,6 @@
 <?php 
 include('../include/conf.php');
-$loginid = $_GET['loginid'];
+$loginid = protect($_GET['loginid']);
 mysql_query("UPDATE loginmaster SET isactive = '1' WHERE id ='$loginid' ");
 header('location:trashaccount.php');
 ?>
